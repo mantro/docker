e9e98b877af9ead61afef65a1b601ca979e02d18
bschuedzig <benjamin.schuedzig@mantro.net>
2017-12-13 15:49:45 +0100

    [BUGFIX] Added missing space for bash-if

--- postgres/docker-entrypoint-initdb.d/init.sh
+++ postgres/docker-entrypoint-initdb.d/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-[ -z "$DBUSER_USER"] && echo "DBUSER_USER is not defined, defaulting to 'john'" && DBUSER_USER=john
-[ -z "$DBUSER_PASS"] && echo "DBUSER_PASS is not defined, defaulting to 'doe'" && DBUSER_PASS=doe
-[ -z "$DBUSER_DB"] && echo "DBUSER_DB is not defined, defaulting to 'sample'" && DBUSER_DB=sample
+[ -z "$DBUSER_USER" ] && echo "DBUSER_USER is not defined, defaulting to 'john'" && DBUSER_USER=john
+[ -z "$DBUSER_PASS" ] && echo "DBUSER_PASS is not defined, defaulting to 'doe'" && DBUSER_PASS=doe
+[ -z "$DBUSER_DB" ] && echo "DBUSER_DB is not defined, defaulting to 'sample'" && DBUSER_DB=sample
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER "$DBUSER_USER";
